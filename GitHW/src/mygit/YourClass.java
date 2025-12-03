ebc34f7754bb42b1490666a8c4039f1264e7cef6
Chawon <Chawon@LAPTOP>
2025-12-03 14:22:58 +0900

    Change in master branch

--- GitHW/src/mygit/YourClass.java
+++ GitHW/src/mygit/YourClass.java
@@ -2,7 +2,8 @@ package mygit;
 
 public class YourClass {
 	
-	public void sayHello(String[] toWhom) {
+	public void sayHello(String toWhom) {
 		System.out.println("Hello!" + toWhom);
+		System.out.println("Your change in master branch");
 	}
 }
